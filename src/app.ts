eecd6a35bee70fa2fa877eabc6bcd85bf0cf69f1
devahmedomar <dev.a7med.omar@gmail.com>
2026-02-17 06:33:23 +0200

    edits cros

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.options("*", cors());
 app.use(express.json());
 
 app.get("/", (_req, res) => {
